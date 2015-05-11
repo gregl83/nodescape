dc45124e7923f1d08aa9cc843f2e8a73672f4d5d
Gregory Langlais <general@gregorylanglais.com>
2015-05-11 01:46:53 -0400

    beta high level todos

--- src/Nodescape.js
+++ src/Nodescape.js
@@ -1,3 +1,11 @@
+// todo research integration with jsdon and selenium
+
+// todo define browser interface (use familiar terminology/interface such as chrome or firefox)
+
+// todo write headless browser with window and tab support (based off jsdom)
+
+// todo publish beta
+
 function Nodescape() {
   // todo
 }
