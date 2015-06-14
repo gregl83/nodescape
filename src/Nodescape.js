1bccd161ccca4369c59d6a71bbd93357d648a6c0
Gregory Langlais <general@gregorylanglais.com>
2015-06-14 18:41:28 -0400

    core depends

--- src/Nodescape.js
+++ src/Nodescape.js
@@ -1,12 +1,22 @@
-// todo research integration with jsdon and selenium
+var config = require('config');
 
 // todo define browser interface (use familiar terminology/interface such as chrome or firefox)
 
-// todo write headless browser with window and tab support (based off jsdom)
+// todo write headless browser with tab support (based off jsdom)
 
 // todo publish beta
 
 function Nodescape() {
+  var self = this;
+
+  // todo consider page cache
+
+  // todo consider developer tools vs standalone jquery
+
+  self.history = [];
+
+  self.location = config.get('location');
+
   // todo
 }
 
---
+++ src/Tab.js
@@ -0,0 +1,15 @@
+/**
+ * Nodescape Tab
+ *
+ * @param {string} start location
+ * @constructor
+ */
+function Tab(start) {
+  var self = this;
+
+  self.location = self.address = start;
+
+  // todo
+}
+
+module.exports = Tab;
